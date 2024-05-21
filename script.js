fe7540d86b16d604579dd22637d4cb2aba83b8ab
ThomasMaizonave <139795025+ThomasMaizonav@users.noreply.github.com>
2024-05-21 15:06:13 -0300

    Atualização Produtos
    Atualizar produtos

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-// Este arquivo pode ser usado para adicionar funcionalidades interativas
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Página carregada');
-});
+/*function changeLanguage(language) {
+    const texts = {
+        'pt-BR': {
+            'description': 'Escolha o idioma:'
+        },
+        'en-US': {
+            'description': 'Choose your language:'
+        }
+    };
+
+    // Atualiza o texto com base no idioma selecionado
+    document.getElementById('title').textContent = texts[language]['title'];
+    document.getElementById('description').textContent = texts[language]['description'];
+
+    // Atualiza o atributo lang da tag html
+    document.documentElement.lang = language;
+}*/
+
