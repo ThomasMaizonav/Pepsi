9c20662bffcc9a203ed0a692c46a4a9ca18464f5
ThomasMaizonave <139795025+ThomasMaizonav@users.noreply.github.com>
2024-05-27 21:47:07 -0300

    Para o GitHub
    Final do projeto, onde ele vai subir ao GitHub!!!

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function imgSlider(imageSrc, color) {
-    document.querySelector('.pepsi').src = imageSrc
-    changeColor(color)
+function imgSlider(imageSrc, color, textColor, spanColor) {
+    document.querySelector('.pepsi').src = imageSrc;
+    changeColor(color, textColor, spanColor);
 }
 
-function changeColor(color) {
-    const sec = document.querySelector('.sec')
-    sec.style.background = color
+function changeColor(backgroundColor, textColor, spanColor) {
+    const sec = document.querySelector('.sec');
+    sec.style.background = backgroundColor;
+    const textBox = document.querySelector('.textBox');
+    textBox.querySelector('h2').style.color = textColor;
+    textBox.querySelector('p').style.color = textColor;
+    textBox.querySelector('a').style.color = textColor;
+    textBox.querySelector('h2 span').style.color = spanColor;
+}
+
+function expandirImagem() {
+    const imagem = document.querySelector('.logos__pepsi');
+    imagem.classList.toggle('expandida');
 }
